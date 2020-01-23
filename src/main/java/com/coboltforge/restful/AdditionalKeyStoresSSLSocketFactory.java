1b5607fb2104f504dc3e6929a9a639b108bdded0
Christian Beier <cb@shoutrlabs.com>
2020-01-23 13:42:16 +0100

    Update license boilerplate in Java sources

--- src/main/java/com/coboltforge/restful/AdditionalKeyStoresSSLSocketFactory.java
+++ src/main/java/com/coboltforge/restful/AdditionalKeyStoresSSLSocketFactory.java
@@ -3,10 +3,29 @@
  *
  * Author Christian Beier
  *
- * Copyright (C) 2012 CoboltForge
+ * Copyright (c) 2020, shoutr labs
+ * All rights reserved.
  *
- * This is proprietary software, all rights reserved!
- * You MUST contact hello@coboltforge.com if you want to use this software in your own product!
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * This one, AdditionalKeyStoresSSLSocketFactory is largely based on public domain code,
  * see http://stackoverflow.com/questions/2642777/trusting-all-certificates-using-httpclient-over-https/6378872#6378872
